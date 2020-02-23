a0398f4db7026b65def18edaf97e01913c0163ef
zakariabijoy <zakaria.bijoy@live.com>
2020-02-24 03:16:41 +0600

    project finised

--- JQueryAjaxInAsp.NetMVC/JQueryAjaxInAsp.NetMVC/Controllers/EmployeesController.cs
+++ JQueryAjaxInAsp.NetMVC/JQueryAjaxInAsp.NetMVC/Controllers/EmployeesController.cs
@@ -1,12 +1,14 @@
-﻿using JQueryAjaxInAsp.NetMVC.Models;
+﻿ using JQueryAjaxInAsp.NetMVC.Models;
 using System;
 using System.Collections.Generic;
-using System.IO;
+ using System.Data.Entity;
+ using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+ using System.Web.Services.Description;
 
-namespace JQueryAjaxInAsp.NetMVC.Controllers
+ namespace JQueryAjaxInAsp.NetMVC.Controllers
 {
     public class EmployeesController : Controller
     {
@@ -19,34 +21,100 @@ namespace JQueryAjaxInAsp.NetMVC.Controllers
         }
         public ActionResult ViewAll()
         {
-           var employees= _context.Employees.ToList();
-            return View(employees);
+           
+            return View(GetAllEmployee());
+        }
+
+        IEnumerable<Employee> GetAllEmployee()
+        {
+            return _context.Employees.ToList<Employee>();
         }
 
         public ActionResult AddOrEdit(int id =0)
         {
             Employee emp = new Employee();
+            if (id != 0)
+            {
+                emp = _context.Employees.FirstOrDefault(e => e.Id == id);
+            }
             return View(emp);
         }
 
         [HttpPost]
        public ActionResult AddOrEdit(Employee emp)
         {
-            if (emp.ImageUpload != null)
+            try
             {
-                string fileName = Path.GetFileNameWithoutExtension(emp.ImageUpload.FileName);
-                string fileExtention = Path.GetExtension(emp.ImageUpload.FileName);
-                fileName = fileName + DateTime.Now.ToString("yymmssfff") + fileExtention;
+                if (emp.ImageUpload != null)
+                {
+                    string fileName = Path.GetFileNameWithoutExtension(emp.ImageUpload.FileName);
+                    string fileExtention = Path.GetExtension(emp.ImageUpload.FileName);
+                    fileName = fileName + DateTime.Now.ToString("yymmssfff") + fileExtention;
 
-                emp.ImagePath = "~/AppFiles/Images/" + fileName;
+                    emp.ImagePath = "~/AppFiles/Images/" + fileName;
 
-                emp.ImageUpload.SaveAs(Path.Combine(Server.MapPath("~/AppFiles/Images/"), fileName));
+                    emp.ImageUpload.SaveAs(Path.Combine(Server.MapPath("~/AppFiles/Images/"), fileName));
 
+                }
+
+                if (emp.Id == 0)
+                {
+                    _context.Employees.Add(emp);
+                    _context.SaveChanges();
+                }
+                else
+                {
+                    _context.Entry(emp).State = EntityState.Modified;
+                    _context.SaveChanges();
+                }
+               
+
+                return Json(new
+                {
+                    success = true,
+                    html = GlobalClass.RenderRazorViewToString(this, "ViewAll",
+                    GetAllEmployee()),
+                    message = "Submitted Successfully"
+                }, JsonRequestBehavior.AllowGet);
             }
-            _context.Employees.Add(emp);
-            _context.SaveChanges();
-            ModelState.Clear();
-            return RedirectToAction("ViewAll");
+            catch (Exception ex)
+            {
+
+                return Json(new
+                {
+                    success = false,
+                    
+                    message = ex.Message
+                }, JsonRequestBehavior.AllowGet);
+            }
         }
+
+       public ActionResult Delete(int id)
+       {
+           try
+           {
+               var emp = _context.Employees.Find(id);
+               _context.Employees.Remove(emp);
+               _context.SaveChanges();
+
+               return Json(new
+               {
+                   success = true,
+                   html = GlobalClass.RenderRazorViewToString(this, "ViewAll",
+                       GetAllEmployee()),
+                   message = "Deleted Successfully"
+               }, JsonRequestBehavior.AllowGet);
+            }
+           catch (Exception ex)
+           {
+               return Json(new
+               {
+                   success = false,
+
+                   message = ex.Message
+               }, JsonRequestBehavior.AllowGet);
+            }
+          
+       }
     }
 }
--- JQueryAjaxInAsp.NetMVC/JQueryAjaxInAsp.NetMVC/Scripts/Scripts.js
+++ JQueryAjaxInAsp.NetMVC/JQueryAjaxInAsp.NetMVC/Scripts/Scripts.js
@@ -18,7 +18,17 @@ function jQueryAjaxPost(form) {
             url: form.action,
             data: new FormData(form),
             success: function (response) {
-                $("#firstTab").html(response);
+                if (response.success) {
+                    $("#firstTab").html(response);
+                    refreshAddNewTab($(form).attr('data-resetUrl'), true);
+                    $.notify(response.message, "success");
+                    if (typeof activatejQueryTable !== 'undefined' && $.isFunction('activatejQueryTable'))
+                        activatejQueryTable();
+                }
+                else {
+                    $.notify(response.message, "error");
+                }
+              
             }
         }
         if ($(form).attr('enctype') == "multipart/form-data") {
@@ -31,3 +41,54 @@ function jQueryAjaxPost(form) {
 
     return false;
 }
+
+function refreshAddNewTab(resetUrl, showViewtab) {
+
+    $.ajax({
+        type:'GET',
+        url: resetUrl,
+        success: function(response) {
+            $("#secondTab").html(response);
+            $('ul.nav.nav-tabs a:eq(1)').html('Add New');
+            if (showViewtab)
+                $('ul.nav.nav-tabs a:eq(0)').tab('show');
+        }
+    });
+}
+
+function Edit(url) {
+
+    $.ajax({
+        type: 'GET',
+        url: url,
+        success: function(response) {
+            $("#secondTab").html(response);
+            $('ul.nav.nav-tabs a:eq(1)').html('Edit');
+            $('ul.nav.nav-tabs a:eq(1)').tab('show');
+
+        }
+    });
+}
+
+function Delete(url) {
+    if (confirm('Are you sure to delete this record?') == true) {
+        $.ajax({
+            type: 'POST',
+            url: url,
+            success: function (response) {
+                if (response.success) {
+                    $("#firstTab").html(response.html);
+                    $.notify(response.message, "warn");
+                    if (typeof activatejQueryTable !== 'undefined' && $.isFunction('activatejQueryTable'))
+                        activatejQueryTable();
+                } else {
+                    $.notify(response.message, "error");
+                }
+              
+               
+
+            }
+        });
+    }
+    }
+    
